5fa099b812297d3ff59e2111cb2641da853f9f55
Ahmed Habib <ahmedboboooooko@gmail.com>
2022-03-23 11:03:30 +0200

    Custom hook, header and hero section

---
+++ components/Hero/Hero.js
@@ -0,0 +1,30 @@
+import { Container, Grid } from '@mui/material'
+import React from 'react'
+import Image from 'next/image'
+import useStyles from './styles'
+import hero from '../../images/Hero_Section_SVG.svg'
+
+export default function Hero() {
+  const classes = useStyles();
+
+  return (
+    <Container  md={6}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>  
+            <Grid xs={12} md={6} className={classes.leftSide}>
+                <h2 className={classes.headLine}>
+                Learn to code,
+                </h2>
+                <h1 className={classes.title}>
+                With a New Digital Life
+                </h1>
+                <p className={classes.description}>
+                Start your new Work with our weekly remotely Tech programs in full-stack developer and build web and mobile apps powered by JavaScript Start your new Work with our weekly remotely
+                </p>
+            </Grid>
+            <Grid  xs={12}md={6} className={classes.rightSide}  >
+               <Image src={hero} alt='learn coding' className={classes.image}/>
+            </Grid>
+        </Grid>
+    </Container>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import HomePage from "./Home/HomePage";
+
 
 export default function Home() {
   return (
@@ -8,6 +13,13 @@ export default function Home() {
         <meta name="description" content="CodifyCollege" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Router>
+      <Header />
+      <Routes>
+          <Route element={<HomePage />} path="/" exact />
+      </Routes>
+      </Router>
+      
     </div>
   );
 }
