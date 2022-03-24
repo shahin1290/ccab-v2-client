4eebcb1a40b31093801c0a583520410dede552cd
el-esnawy <melesnawy@gmail.com>
2022-03-25 02:40:38 +0300

    how to start section donw

--- components/Homepage/HowToStart/index.js
+++ components/Homepage/HowToStart/index.js
@@ -1,20 +1,82 @@
-import { Container, Stack, Box, Card, CardMedia } from "@mui/material";
+import { Container, Stack, Box, Card, CardMedia, CardContent, Grid } from "@mui/material";
 import Image from "next/image";
+import { CalenderIcon, HelpIcon, HouseIcon, TeacherIcon } from "../../../images/svg/cardIcons";
 
-import { PrimaryButton, SectionHeader, SectionSubHeader, CustomImage } from "../../shared/CustomComponents";
+import {
+  PrimaryButton,
+  SectionHeader,
+  SectionSubHeader,
+  CustomImage,
+  CustomCard,
+  CustomCardContent,
+} from "../../shared/CustomComponents";
 
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const cardData = [
+  {
+    id: 1,
+    icon: HouseIcon,
+    title: "Learn full-stack from the comfort of your own home",
+    content:
+      "Get access to all of the course's core contents, including both text and video lessons.  Begin learning at your own pace from the first day of the Bootcamp, extending the course duration as needed.",
+  },
+  {
+    id: 2,
+    icon: TeacherIcon,
+    title: "Personal 1:1 sessions with mentors",
+    content:
+      "Schedule weekly 1:1 appointments with your mentor to work through any current challenges, obtain additional explanations, and ask any concerns you may have.",
+  },
+  {
+    id: 3,
+    icon: HelpIcon,
+    title: "For code-related questions, there is a support platform",
+    content: "The curriculum, projects, exercises, and outcomes are identical to the boot camp's in-person version.",
+  },
+  {
+    id: 4,
+    icon: CalenderIcon,
+    title: "Flexible schedule Completely",
+    content: "We provide support via a support portal, chat, and provide comments on your progress.",
+  },
+];
 const HowToStart = () => {
+  const theme = useTheme();
+  const lg = useMediaQuery(theme.breakpoints.down("lg"));
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <Box sx={{ my: 5, bgcolor: "secondary.background", py: 10 }}>
-      <Container>
-        <Stack justifyContent="center" alignItems="center" sx={{ mb: 10 }} spacing={2}>
-          <SectionHeader variant="h4">How can you start?</SectionHeader>
-          <SectionSubHeader>
-            Register yourself now and get real employable skills that top companies want
-          </SectionSubHeader>
-        </Stack>
-        <Stack direction="row" sx={{ width: "100%", justifyContent: "space-between" }}>
-          <Stack spacing={2} justifyContent="center" alignItems="start" sx={{ maxWidth: "450px", height: "100%" }}>
+    <Container
+      sx={{
+        bgcolor: "secondary.background",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        my: 5,
+        py: 10,
+        gap: 10,
+
+        [theme.breakpoints.down("md")]: {
+          my: 1,
+          py: 3,
+          gap: 5,
+        },
+      }}>
+      <Stack justifyContent="center" alignItems="center" spacing={2}>
+        <SectionHeader variant="h4">How can you start?</SectionHeader>
+        <SectionSubHeader>
+          Register yourself now and get real employable skills that top companies want
+        </SectionSubHeader>
+      </Stack>
+      <Grid container justifyContent={lg ? "center" : "space-between"} spacing={5}>
+        <Grid item md={5}>
+          <Stack
+            spacing={2}
+            justifyContent="center"
+            alignItems={md ? "center" : "start"}
+            sx={{ maxWidth: "450px", height: "100%" }}>
             <SectionHeader variant="h5">Remote supervised self-paced training</SectionHeader>
             <SectionSubHeader>
               Choose a start date and register on this page to secure your spot. To prepare for the Bootcamp, get the
@@ -25,17 +87,27 @@ const HowToStart = () => {
               Learn More
             </PrimaryButton>
           </Stack>
-          <Box>
-            <CustomImage src="/howToStart.png" alt="How To Start Image" />
-          </Box>
-        </Stack>
-        <Stack direction="row">
-          <Card>
-            <CardMedia component="img" />
-          </Card>
-        </Stack>
-      </Container>
-    </Box>
+        </Grid>
+        <Grid item md={5}>
+          <CustomImage src="/howToStart.png" alt="How To Start Image" />
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="space-around" gap={2} columns={14}>
+        {cardData.map((item) => {
+          return (
+            <Grid item key={item.id} lg={3} md={5} sm={6} xs={14}>
+              <CustomCard>
+                <CustomCardContent>
+                  <item.icon />
+                  <SectionHeader sx={{ fontSize: "18px", my: 2, textAlign: "center" }}>{item.title}</SectionHeader>
+                  <SectionSubHeader sx={{ fontSize: "14px" }}>{item.content}</SectionSubHeader>
+                </CustomCardContent>
+              </CustomCard>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
--- components/shared/CustomComponents.js
+++ components/shared/CustomComponents.js
@@ -1,12 +1,12 @@
 import { styled } from "@mui/system";
 
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Card } from "@mui/material";
 import Image from "next/image";
 
 const SectionHeader = styled(Typography)(({ theme }) => ({
   color: theme.palette.mainTextColor,
   fontFamily: "'Segoe UI', sans-serif ",
-  fontWeight: "bold !important",
+  fontWeight: "bold",
 }));
 
 const SectionSubHeader = styled(Typography)(({ theme }) => ({
@@ -24,7 +24,28 @@ const PrimaryButton = styled(Button)(({ theme }) => ({
 
 const CustomImage = styled("img")(({ theme }) => ({
   borderRadius: 20,
-  boxShadow: `-30px 30px 0px ${theme.palette.primary.light} `,
+  boxShadow: `-30px 30px 0px ${theme.palette.primary.light}`,
+  width: "100%",
+  height: "auto",
+  [theme.breakpoints.down("md")]: {
+    boxShadow: `none`,
+  },
 }));
 
-export { SectionHeader, SectionSubHeader, PrimaryButton, CustomImage };
+const CustomCard = styled(Card)(({ theme }) => ({
+  borderRadius: 20,
+  // width: "300px",
+  height: "364px",
+  boxShadow: "0 0 20px  #00000029",
+}));
+const CustomCardContent = styled(Card)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "start",
+  alignItems: "center",
+  padding: "30px 20px",
+  border: " none",
+  height: "100%",
+}));
+
+export { SectionHeader, SectionSubHeader, PrimaryButton, CustomImage, CustomCard, CustomCardContent };
