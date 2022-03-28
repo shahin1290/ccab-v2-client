2b88ffda3bc52b2988a559e4f9860d7e8a1bb7e4
Ahmed Habib <ahmedboboooooko@gmail.com>
2022-03-28 07:18:00 +0200

    First Hero section changes

--- components/Homepage/Hero/styles.js
+++ components/Homepage/Hero/styles.js
@@ -1,58 +1,80 @@
-// import { makeStyles } from '@material-ui/core/styles';
+import {  styled } from '@mui/system';
+import { Container, Grid } from "@mui/material";
+import Image from 'next/image';
 
-// export default makeStyles((theme) => ({
-//     container:{
-//         marginTop:'100px',
-//     },
-//     leftSide:{
-//         marginTop:'140px',
-//         [theme.breakpoints.down('sm')]:{
-//             marginTop:'10px',
-//         }
-//     },
-//     rightSide:{
-//         marginTop:'60px',
-//         [theme.breakpoints.down('sm')]:{
-//             marginTop:'0',
-//         }
-//     },
-//     headLine:{
-//         textAlign: 'left',
-//         color: '#707070',
-//         fontSize:"45px",
-//         marginBottom:'-20px',
-//         whiteSpace:'nowrap',
-//         [theme.breakpoints.down('sm')]:{
-//             fontSize:"30px",
-//         }
-//     },
-//     title:{
-//         fontSize:"48px",
-//         textAlign: 'left',
-//         color: '#6364D9',
-//         whiteSpace:'nowrap',
-//         marginBottom:'-5px',
-//         [theme.breakpoints.down('sm')]:{
-//             fontSize:"35px",
-//             whiteSpace:'break-spaces',
-//         }
 
-//     },
-//     description:{
-//         fontSize:"18px",
-//         textAlign: 'left',
-//         color: '#707070',
-//         lineHeight:'1.5',
-//         [theme.breakpoints.down('sm')]:{
-//             fontSize:"15px",
-//         }
-//     },
-//     image:{
-//         textAlign:"center",
-//         height:'100%',
-//         width:'100%',
-//         [theme.breakpoints.down('sm')]:{
-//             textAlign:"left",
-//         }
-//     }
-// }))
+export const HeroContainer = styled(Container)({
+    background:"#f5f6f9",
+    maxWidth:"none !important",
+    fontFamily:'Segoe UI',
+    "@media (max-width: 480px)":{
+        fontSize:"5px",
+}
+})
+
+export const LeftSide = styled(Grid)({
+        marginTop:'120px',
+        zIndex:'1',
+        "@media (max-width: 480px)":{
+            marginTop:'10px',
+}
+})
+
+export const RightSide = styled(Grid)({
+    marginTop:'60px',
+    "@media (max-width: 480px)":{
+        marginTop:'0',
+    }
+})
+
+
+export const HeadLine = styled('h2')({
+        textAlign: 'left',
+        color: '#707070',
+        fontSize:"45px",
+        marginBottom:'-40px',
+        marginLeft:"30px",
+        whiteSpace:'nowrap',
+        "@media (max-width: 600px)":{
+            fontSize:"25px",
+            marginBottom:'-5px',
+            marginLeft:"0",
+        }
+
+})
+
+
+export const Title = styled('h1')({
+        fontSize:"48px",
+        textAlign: 'left',
+        color: '#6364D9',
+        whiteSpace:'nowrap',
+        marginLeft:"30px",
+        marginBottom:'-5px',
+        "@media (max-width: 600px)":{
+            whiteSpace:'break-spaces',
+            fontSize:"26px",
+            marginLeft:"0",
+}
+})
+
+export const Description = styled('p')({
+        fontSize:"18px",
+        textAlign: 'left',
+        color: '#707070',
+        lineHeight:'1.5',
+        marginLeft:"30px",
+        "@media (max-width: 600px)":{
+            fontSize:"15px",
+            marginLeft:"0",
+        }
+})
+
+export const Media = styled(Image)({
+        textAlign:"center",
+        height:'100%',
+        width:'100%',
+        "@media (max-width: 600px)":{
+            textAlign:"left",
+}
+})
--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      {/* <Hero /> */}
+      <Hero />
       <HowToStart />
       <WhyJavascript />
       <Tuition />
