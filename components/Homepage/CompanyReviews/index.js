b45a8b2cd9124941076e238377de9730bdf53f26
shahin1290 <s.patowary@yahoo.com>
2022-03-29 22:09:25 +0200

    finishes company review styling

--- components/Homepage/CompanyReviews/index.js
+++ components/Homepage/CompanyReviews/index.js
@@ -1,22 +1,133 @@
-import { Container } from "@mui/material";
-import React from "react";
+import { useTheme } from "@emotion/react";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import React, { useEffect } from "react";
 
-const CompanyReviews = () => {
+const RatingSection = () => {
+  const theme = useTheme();
+
   return (
-    <Container
-      maxWidth="xl"
+    <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        pt: { xs: 3, md: 10 },
-        pb: { xs: 2 },
+        background: theme.palette.secondary.main,
+        py: { xs: 3, md: 10 },
       }}
     >
-      reviews
-    </Container>
+      <Container maxWidth="xl">
+        <Grid container spacing={10} sx={{ width: "100%" }}>
+          {data.map((item) => (
+            <Grid item md={3} xs={12} key={item.id}>
+              <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                sx={{
+                  backgroundColor: "secondary.background",
+                  py: 5,
+                  borderRadius: "20px",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Image
+                    src={item.imageUrl}
+                    width={80}
+                    height={70}
+                    alt="switchup"
+                  />
+                </Box>
+
+                <Typography
+                  align="center"
+                  variant="h6"
+                  sx={{ fontWeight: "normal", fontSize: { xs: 16 } }}
+                >
+                  {item.rating}/5 Rating
+                </Typography>
+
+                <Box
+                  sx={{
+                    position: "relative",
+                    pb: 1,
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  <svg
+                    fill="#FFC107"
+                    viewBox="0 0 576 512"
+                    width="2.2rem"
+                    title="star"
+                  >
+                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                  </svg>
+                  <svg
+                    fill="#FFC107"
+                    viewBox="0 0 576 512"
+                    width="2.2rem"
+                    title="star"
+                  >
+                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                  </svg>
+                  <svg
+                    fill="#FFC107"
+                    viewBox="0 0 576 512"
+                    width="2.2rem"
+                    title="star"
+                  >
+                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                  </svg>
+                  <svg
+                    fill="#FFC107"
+                    viewBox="0 0 576 512"
+                    width="2.2rem"
+                    title="star"
+                  >
+                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                  </svg>
+                  <svg
+                    fill="#FFC107"
+                    viewBox="0 0 576 512"
+                    width="2.2rem"
+                    title="star"
+                  >
+                    <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+                  </svg>
+
+                  <Box
+                    sx={{
+                      background: "white",
+                      height: "100%",
+                      overflow: "hidden",
+                      "mix-blend-mode": "color",
+                      position: "absolute",
+                      top: 0,
+                      right: 0,
+                    }}
+                    style={{
+                      width: `${(1 - item.rating / 5) * 100}%`,
+                    }}
+                  ></Box>
+                </Box>
+              </Stack>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
-export default CompanyReviews;
+const data = [
+  { id: 1, imageUrl: "/facebook.png", rating: 4.5 },
+  { id: 2, imageUrl: "/google.png", rating: 4.5 },
+  { id: 3, imageUrl: "/switchup.png", rating: 4.5 },
+  { id: 4, imageUrl: "/trustpilot.jpg", rating: 4 },
+];
+
+export default RatingSection;
--- components/StudentReviews/index.js
+++ components/StudentReviews/index.js
@@ -9,14 +9,13 @@ import { useState } from "react";
 import { styled } from "@mui/system";
 import Underline from "../../images/svg/Underline";
 import Image from "next/image";
+import { Calculate } from "@mui/icons-material";
 
 const UserImg = styled("img")(({ theme }) => ({
-  width: "100px",
-  height: "100px",
+  display: "block",
+  width: "10em",
+  height: "10em",
   borderRadius: "50%",
-  position: "absolute",
-  bottom: "-50px",
-  left: "50%",
 }));
 
 function Arrow(props) {
@@ -35,8 +34,8 @@ function Arrow(props) {
 }
 
 const CustomImage = styled("img")(({ theme }) => ({
-  borderRadius: 10,
-  width: "55%",
+  borderRadius: 20,
+  width: "50%",
   height: "auto",
   margin: "0 auto",
 }));
@@ -77,60 +76,213 @@ const StudentReviews = () => {
         </Typography>
       </Box>
 
-      <Box sx={{ position: "relative", }}>
+      <Box sx={{ position: "relative" }}>
         <Slider {...settings} ref={(slider) => setSlider(slider)}>
-          <Box sx={{ position: "relative" }}>
-            <CustomImage
-              src="/howToStart.png"
-              alt="How To Start Image"
-              onClick={handleOpen}
-            />
-            <Box
-              sx={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-              }}
-            >
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="10em"
-                height="10em"
+          <Box
+            sx={{
+              position: "relative",
+              paddingBottom: "100px",
+            }}
+          >
+            <Box sx={{ position: "relative" }}>
+              <CustomImage src="/review-img.jpg" alt="Student Review Image" />
+              <Box
+                onClick={handleOpen}
+                sx={{
+                  position: "absolute",
+                  top: "calc(50% - 50px)",
+                  left: "calc(50% - 50px)",
+                  cursor: "pointer",
+                }}
               >
-                <path
-                  fill="white"
-                  stroke="red"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"
-                ></path>
-              </svg>
+                <svg
+                  focusable="false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="10rem"
+                  height="10rem"
+                >
+                  <path
+                    fill="white"
+                    stroke="none"
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"
+                  ></path>
+                </svg>
+              </Box>
+              <Stack
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
+                sx={{
+                  position: "absolute",
+                  bottom: "-100px",
+                  left: "calc(50% - 50px)",
+                }}
+              >
+                <UserImg src="/person.jpg" alt="student" />
+                <Typography
+                  align="center"
+                  variant="h3"
+                  sx={{
+                    color: theme.palette.primary.main,
+                    fontWeight: "normal",
+                    fontSize: { xs: 22 },
+                  }}
+                >
+                  Ema Watsom
+                </Typography>
+                <Typography
+                  align="center"
+                  variant="h6"
+                  sx={{ fontWeight: "normal", fontSize: { xs: 16 } }}
+                >
+                  London, UK
+                </Typography>
+              </Stack>
             </Box>
-
-            <UserImg src="/person.jpg" alt="student" />
           </Box>
-          <Box>
-            <CustomImage
-              src="/howToStart.png"
-              alt="How To Start Image"
-              onClick={handleOpen}
-            />
+          <Box
+            sx={{
+              position: "relative",
+              paddingBottom: "50px",
+            }}
+          >
+            <Box sx={{ position: "relative" }}>
+              <CustomImage src="/review-img.jpg" alt="Student Review Image" />
+              <Box
+                onClick={handleOpen}
+                sx={{
+                  position: "absolute",
+                  top: "calc(50% - 50px)",
+                  left: "calc(50% - 50px)",
+                  cursor: "pointer",
+                }}
+              >
+                <svg
+                  focusable="false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="10rem"
+                  height="10rem"
+                >
+                  <path
+                    fill="white"
+                    stroke="none"
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"
+                  ></path>
+                </svg>
+              </Box>
+              <Stack
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
+                sx={{
+                  position: "absolute",
+                  bottom: "-100px",
+                  left: "calc(50% - 50px)",
+                }}
+              >
+                <UserImg src="/person.jpg" alt="student" />
+                <Typography
+                  align="center"
+                  variant="h3"
+                  sx={{
+                    color: theme.palette.primary.main,
+                    fontWeight: "normal",
+                    fontSize: { xs: 22 },
+                  }}
+                >
+                  Ema Watsom
+                </Typography>
+                <Typography
+                  align="center"
+                  variant="h6"
+                  sx={{ fontWeight: "normal", fontSize: { xs: 16 } }}
+                >
+                  London, UK
+                </Typography>
+              </Stack>
+            </Box>
           </Box>
-          <Box>
-            <CustomImage
-              src="/howToStart.png"
-              alt="How To Start Image"
-              onClick={handleOpen}
-            />
+          <Box
+            sx={{
+              position: "relative",
+              paddingBottom: "50px",
+            }}
+          >
+            <Box sx={{ position: "relative" }}>
+              <CustomImage src="/review-img.jpg" alt="Student Review Image" />
+              <Box
+                onClick={handleOpen}
+                sx={{
+                  position: "absolute",
+                  top: "calc(50% - 50px)",
+                  left: "calc(50% - 50px)",
+                  cursor: "pointer",
+                }}
+              >
+                <svg
+                  focusable="false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="10rem"
+                  height="10rem"
+                >
+                  <path
+                    fill="white"
+                    stroke="none"
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"
+                  ></path>
+                </svg>
+              </Box>
+              <Stack
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
+                sx={{
+                  position: "absolute",
+                  bottom: "-100px",
+                  left: "calc(50% - 50px)",
+                }}
+              >
+                <UserImg src="/person.jpg" alt="student" />
+                <Typography
+                  align="center"
+                  variant="h3"
+                  sx={{
+                    color: theme.palette.primary.main,
+                    fontWeight: "normal",
+                    fontSize: { xs: 22 },
+                  }}
+                >
+                  Ema Watsom
+                </Typography>
+                <Typography
+                  align="center"
+                  variant="h6"
+                  sx={{ fontWeight: "normal", fontSize: { xs: 16 } }}
+                >
+                  London, UK
+                </Typography>
+              </Stack>
+            </Box>
           </Box>
         </Slider>
-        <Box sx={{ position: "absolute", top: "50%", right: 0 }}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "calc(50% - 50px)",
+            right: "150px",
+          }}
+        >
           <SliderButton onClick={() => slider.slickNext()}>
             <ArrowForwardIosIcon />
           </SliderButton>
         </Box>
 
-        <Box sx={{ position: "absolute", top: "50%" }}>
+        <Box
+          sx={{ position: "absolute", top: "calc(50% - 50px)", left: "150px" }}
+        >
           <SliderButton onClick={() => slider.slickPrev()}>
             <ArrowBackIosNewIcon color="white" />
           </SliderButton>
