ecf4153835c74272bfbeb2adb3f4107010cc4241
el-esnawy <melesnawy@gmail.com>
2022-04-01 16:43:08 +0300

    updated _app file

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import Head from "next/head";
-
 import HomePage from "../components/Homepage";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Codify College Dashboard</title>
-        <meta name="description" content="CodifyCollege" />
-        <link rel="icon" href="/Logo.ico" />
-      </Head>
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
