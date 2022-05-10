052d9471d0537b34648f7081089f8a0ccd0686cf
shahin1290 <s.patowary@yahoo.com>
2022-05-10 17:02:06 +0200

    fixes techstack styling with design

--- components/pages/Homepage/StudyTime/CustomScheduleCard.js
+++ components/pages/Homepage/StudyTime/CustomScheduleCard.js
@@ -5,8 +5,8 @@ const CustomScheduleCard = ({ period, time, color }) => {
   return (
     <Stack
       spacing={1.5}
-      py={{ xs: 1.5, lg: 3 }}
-      px={{ xs: 1.5, lg: 3, xl: 10 }}
+      py={{ xs: 1.5, md: 5 }}
+      px={{ xs: 1.5, md: 3, xl: 5 }}
       sx={{
         boxShadow: "0 0 25px rgba(0,0,0,0.3)",
         borderRadius: "20px",
@@ -25,7 +25,7 @@ const CustomScheduleCard = ({ period, time, color }) => {
         sx={{
           borderBottomWidth: 4,
           backgroundColor: color,
-          width: { sm: "130px", md: "130px", xl: "150px" },
+          width: { sm: "130px", md: "200px", lg: "250px", },
         }}
       />
       <Typography align="center">{time}</Typography>
