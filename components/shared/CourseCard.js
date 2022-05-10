9cdacad5068b0cfa0d075509e47b1ac0c723b206
Ahmed Habib <ahmedboboooooko@gmail.com>
2022-05-10 19:34:00 +0200

    fixing bugs

--- components/shared/CourseCard.js
+++ components/shared/CourseCard.js
@@ -2,40 +2,45 @@ import { Card, CardContent, Typography } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
-import { CardButton } from "./StyledComponents";
+import { CardButton, ImageWrapper, TextWrapper } from "./StyledComponents";
 
 const CourseCard = ({ image, title, description, slug }) => {
   const router = useRouter();
   return (
     <Card
-      sx={{ maxWidth: 400 }}
+      sx={{ maxWidth: 345, height:"100%"  }}
       style={{
+        display: 'flex',
+        flexDirection:'column',
         background: "#F7F8FB",
         borderRadius: "20px",
         boxShadow: "none",
       }}
     >
+      <ImageWrapper>
       <Image
         priority
         src={image}
         alt="learning"
         layout="responsive"
-        width={385}
+        width={375}
         height={250}
       />
-      <CardContent style={{ textAlign: "center" }}>
-        <Typography variant="h6" align="center" mb={2}>
+      </ImageWrapper>
+      <CardContent style={{ textAlign: "center",  }} >
+        <Typography variant="h6" align="center" mb={2}  >
           {title}
         </Typography>
         <Typography
           variant="body2"
-          sx={{ color: "secondary.contrastText", padding: "0.8rem" }}
+          sx={{ color: "secondary.contrastText", padding: "0.8rem",   }}
         >
           {description}
         </Typography>
         <CardButton
           variant="contained"
           color="primary"
+          style={{}}
           onClick={() => router.push(`/home/course-details/${slug}`)}
         >
           Learn More
